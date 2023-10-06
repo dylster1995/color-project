813540ee5e8459a9e34fa2750b1103eb1fc03222
unknown <dylanyamashiro@gmail.com>
2023-10-06 00:58:25 -0700

    Adjust text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 import './ColorBox.css';
 
 const ColorBox = ({ color, name, paletteId = null, colorId }) => {
+    const darkColorThreshold = .08;
+    const isDarkColor = chroma(color).luminance() <= darkColorThreshold;
+    const lightColorThreshold = .6;
+    const isLightColor = chroma(color).luminance() >= lightColorThreshold;
+    
     function handleCopy(evt) {
         setShowOverlay(true);
         setTimeout( () => setShowOverlay(false), 1500 )
@@ -13,20 +19,20 @@ const ColorBox = ({ color, name, paletteId = null, colorId }) => {
             <CopyToClipboard text={color} onCopy={handleCopy}>
                 <div className='ColorBox' style={{backgroundColor: color}}>
                     <div className={`copy-overlay ${showOverlay ? 'show' : ''}`} style={{backgroundColor: color}} />
-                    <div className={`copy-msg ${showOverlay ? 'show' : ''}`}>
+                    <div className={`copy-msg ${showOverlay ? 'show' : ''} ${isLightColor && 'dark-font'}`}>
                         <h1>copied!</h1>
                         <p>{color}</p>
                     </div>
                     <div className='copy-container'>
                         <div className='box-content'>
-                            <span className='box-content-name'>{name}</span>
+                            <span className={`box-content-name ${ isDarkColor && 'light-font'}`}>{name}</span>
                         </div>
-                        <button className='copy-button'>Copy</button>
+                        <button className={`copy-button ${isLightColor && 'dark-font'}`}>Copy</button>
                     </div>
                     {
                         paletteId != null && (
                         <Link to={`/palette/${paletteId}/${colorId}`} onClick={ evt => evt.stopPropagation()}>
-                            <span className='see-more'>More</span>
+                            <span className={`see-more ${isLightColor && 'dark-font'}`}>More</span>
                         </Link>
                         )
                     }
